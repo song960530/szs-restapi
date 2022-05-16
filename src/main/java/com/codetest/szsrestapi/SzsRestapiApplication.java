ea1749fda93b9fb61eb30caff86e7f3af916ed9b
송문준 <song960530@naver.com>
2022-05-16 14:58:59 +0900

    feat: 최초 생성

---
+++ src/main/java/com/codetest/szsrestapi/SzsRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.codetest.szsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzsRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetest/szsrestapi/SzsRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetest.szsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
